502a7e3b40c1fb35f3ffd6a81aebc327ff15373d
SongHyunmsoo <y85858@naver.com>
2024-01-05 11:09:44 +0900

    2024/01/05 파일 서비스 만들기

--- src/main/java/org/choongang/file/entities/FileInfo.java
+++ src/main/java/org/choongang/file/entities/FileInfo.java
@@ -9,34 +9,32 @@ import org.choongang.commons.entities.BaseMember;
 
 import java.util.UUID;
 
-
 @Data
 @Entity
 @Builder
 @NoArgsConstructor @AllArgsConstructor
 @Table(indexes = {
-        @Index(name = "idx_fInfo_gid",columnList = "gid"),
-        @Index(name = "idx_fInfo_gid_loc",columnList = "gid,location")
+        @Index(name="idx_fInfo_gid", columnList = "gid"),
+        @Index(name="idx_fInfo_gid_loc", columnList = "gid,location")
 })
 public class FileInfo extends BaseMember {
-
     @Id @GeneratedValue
     private Long seq; // 파일 등록 번호, 서버에 업로드하는 파일명 기준
 
-    @Column(length = 65, nullable = false)
+    @Column(length=65, nullable = false)
     private String gid = UUID.randomUUID().toString();
 
-    @Column(length = 65)
+    @Column(length=65)
     private String location;
 
-    @Column(length = 80)    // 파일이름
+    @Column(length=80)
     private String fileName;
 
-    @Column(length = 30)    //
+    @Column(length=30)
     private String extension;
 
+    @Column(length=65)
+    private String fileType;
 
-    private boolean done;   // 임시저장
-
-
+    private boolean done;
 }
